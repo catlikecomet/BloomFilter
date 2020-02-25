fb73e198bbf1c511c6acd5e68058f284b03993ea
Cameron-Jade Mummery <cami.mummery@aexp.com>
2020-02-25 11:18:03 +0000

    hmm

--- src/BloomFilter.java
+++ src/BloomFilter.java
@@ -6,6 +6,8 @@ public class BloomFilter {
     public boolean[] bitmap = new boolean[256];
     MessageDigest md = MessageDigest.getInstance("MD5");
     byte[] result;
+    double random = Math.random();
+    int iRandom = (int) random;
 
     public BloomFilter() throws NoSuchAlgorithmException{}
 
@@ -13,16 +15,14 @@ public class BloomFilter {
         //hashing the values
         result = md.digest(value.getBytes());
 
-        double random = Math.random();
-
-            bitmap[result[(int)random] + 128] = true;
-            bitmap[result[(int)random] + 128] = true;
-            bitmap[result[(int)random] + 128] = true;
+            bitmap[result[iRandom] + 128] = true;
+            bitmap[result[iRandom] + 128] = true;
+            bitmap[result[iRandom] + 128] = true;
     }
 
     boolean contains(String value) {
         result = md.digest(value.getBytes());
-        if (bitmap[result[0] + 128] && bitmap[result[5] + 128] && bitmap[result[9] + 128]) {
+        if (bitmap[result[iRandom] + 128]) {
             return true;
         }
         return false;
